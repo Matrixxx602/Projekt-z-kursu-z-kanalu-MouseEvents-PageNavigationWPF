e2e4a836b76e0c8c791e06afe1b0ec77a84a6dd8
matrixxx602 <159135973+Matrixxx602@users.noreply.github.com>
2024-06-06 17:06:52 +0200

    Dodanie projektu.

---
+++ SimpleNavigation/SimpleNavigation/MainVM.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleNavigation
+{
+    public class MainVM : ObservableObject
+    {
+        private PageId _pageId;
+
+        public PageId PageId
+        {
+            get { return _pageId; }
+            set { SetProperty(ref _pageId, value); }
+        }
+
+        public ICommand CMDChangePage => new RelayCommand<PageId>(ChangePage);
+
+        void ChangePage(PageId newPage)
+        { 
+            PageId = newPage;
+        }
+
+        public MainVM() 
+        {
+            PageId = PageId.A;
+        }
+        
+    }
+}
